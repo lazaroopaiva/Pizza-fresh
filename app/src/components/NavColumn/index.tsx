842fbfa088e5fccaee056fd2e9b7f4b9eee94269
lazaroopaiva <paivalazaro2017@gmail.com>
2022-10-06 22:59:07 -0300

    Commit final

--- app/src/components/NavColumn/index.tsx
+++ app/src/components/NavColumn/index.tsx
@@ -5,6 +5,7 @@ import * as S from "./style";
 import { RoutePath } from "types/routes";
 import NavColumnItem from "components/NavColumnItem";
 import { HTMLAttributes } from "react";
+import { useNavigate } from "react-router-dom";
 
 type NavColumnType = HTMLAttributes<HTMLDivElement>;
 
@@ -12,7 +13,9 @@ type NavColumnProps = {
   activeRoute: RoutePath;
 } & NavColumnType;
 
-const NavColumn = ( { activeRoute }: NavColumnProps ) => {
+const NavColumn = ({ activeRoute }: NavColumnProps) => {
+  const navigate = useNavigate();
+
   const items = [
     {
       icon: <Market />,
@@ -33,10 +36,11 @@ const NavColumn = ( { activeRoute }: NavColumnProps ) => {
       navigation: RoutePath.SETTINGS_USERS,
     },
   ];
-  return <S.NavColumn>
-      <S.NavColumn>
+  return (
+    <S.NavColumn>
       {items.map((item, key) => (
         <NavColumnItem
+          onClick={() => navigate(item.navigation)}
           active={item.navigation === activeRoute}
           icon={item.icon}
           title={item.title}
@@ -45,7 +49,7 @@ const NavColumn = ( { activeRoute }: NavColumnProps ) => {
         />
       ))}
     </S.NavColumn>
-  </S.NavColumn>;
+  );
 };
 
 export default NavColumn;
--- app/src/types/OrderItemType.ts
+++ app/src/types/OrderItemType.ts
@@ -1,7 +1,7 @@
-import { ProductResponse } from "./Product";
+import { ProductResponse } from "types/api/product";
 
 export interface OrderItemType {
-  product: ProductResponse;
-  quantity: number;
-  observation?: string;
+	product: ProductResponse;
+	quantity: number;
+	observation?: string;
 }
